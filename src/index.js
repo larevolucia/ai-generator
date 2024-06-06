bdf4de42c057801dae208e5be0706192f504c4d9
larevolucia <reis.lucia@gmail.com>
2024-06-06 15:37:18 +0200

    call AI API

--- src/index.js
+++ src/index.js
@@ -4,15 +4,29 @@ function generatePoem(event) {
   let poemElement = document.querySelector("#poem");
   poemElement.style.setProperty("display", "block");
 
-  let formInput = document.querySelector("#search-input");
-  console.log(formInput.value);
+  let apiKey = "ab8aa73684ae7b075f37aoat0358dd04";
+  let context =
+    "You are an AI assistant that generates poems in Brazilian portuguese. Poems should be given in HTML format. Poems mustn't have titles and must contain at least 3 lines.";
+  let prompt = document.querySelector("#search-input").value;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log(prompt);
 
+  axios.get(apiUrl).then(showPoem);
+}
+
+function showPoem(response) {
+  console.log(response.data.answer);
   new Typewriter("#poem", {
-    strings: `${formInput.value}`,
+    strings: `${response.data.answer}`,
     autoStart: true,
+    delay: 65,
     cursor: null
   });
 }
 
+//Generate Poem from Input //
+
 let poemForm = document.querySelector("#poem-generator-form");
 poemForm.addEventListener("submit", generatePoem);
+
+// AI API //
