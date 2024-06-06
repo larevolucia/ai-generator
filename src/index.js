b247e258d911784930dd2692fbb237d5f45b4b3f
larevolucia <reis.lucia@gmail.com>
2024-06-06 17:22:47 +0200

    added poem type to form and spinner

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
 function generatePoem(event) {
   event.preventDefault();
 
+  //Select poem element //
+
   let poemElement = document.querySelector("#poem");
+
+  //Display loading //
+
   poemElement.style.setProperty("display", "block");
+  poemElement.innerHTML = `<dotlottie-player
+      src="https://lottie.host/2b956c1a-7c39-40c9-83e6-e0fec54533ec/haODh86Q6v.json"
+      background="transparent"
+      speed="1"
+      style="width: 70px; height: 70px"
+      loop
+      autoplay
+    ></dotlottie-player>`;
 
-  new Typewriter("#poem", {
-    strings: "Generating poem...",
-    autoStart: true,
-    delay: 1.5,
-    cursor: null
-  });
+  // call AI API //
 
-  // AI API //
   let apiKey = "ab8aa73684ae7b075f37aoat0358dd04";
-  let context =
-    "You are an AI assistant that generates poems in Brazilian portuguese. Poems should be given in HTML format. Poems mustn't have titles and must contain at least 3 lines.";
+  let poemType = document.querySelector("#poem-types");
+  let context = `You are an AI assistant that generates poems. Please, create a ${poemType.value} in Brazilian Portuguese. Poems shall be displayed in HTML format, following the example: <p>This is a verse</p>`;
   let prompt = document.querySelector("#search-input").value;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log(prompt);
+
+  console.log(`Generating ${poemType.value} about ${prompt}`);
 
   axios.get(apiUrl).then(showPoem);
 }
 
 function showPoem(response) {
-  console.log(response.data.answer);
+  console.log(response.data);
+
   new Typewriter("#poem", {
     strings: `${response.data.answer}`,
     autoStart: true,
